0edac34cd36f9a2f475da6e3d62470cd31a30307
Yuri Fedorov <yuri.inpost@gmail.com>
2022-01-10 23:11:27 +0300

    show/hide sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'yurifedorov-cv';
+  isShown: boolean = false ; // hidden by default
+
+  toggleShow() {
+    this.isShown = ! this.isShown;
+  }
 }
