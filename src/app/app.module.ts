dece1923069fdd671faac3788c2704ff499df580
Yuri Fedorov <yuri.inpost@gmail.com>
2022-01-11 11:19:30 +0300

    add courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkExperienceComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
