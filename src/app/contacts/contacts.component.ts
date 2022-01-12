f32097572915be9926267ba4d1dd69ce475f8f59
Yuri Fedorov <yuri.inpost@gmail.com>
2022-01-12 11:30:58 +0300

    edit project button

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ContactsComponent {
+  contacts: any[] = [
+    {
+      "icon": "fas fa-mobile-alt",
+      "value": "+7 (921) 753-02-95",
+      "link": 'tel:+7-921-753-0295'
+    },
+    {
+      "icon": "fas fa-envelope",
+      "value": "yuri.inpost@gmail.com",
+      "link": 'mailto:yuri.inpost@gmail.com'
+    },
+    {
+      "icon": "fab fa-telegram",
+      "value": "@yuryfyodorov",
+      "link": '#'
+    },
+    {
+      "icon": "fab fa-linkedin",
+      "value": "Linkedin",
+      "link": 'https://www.linkedin.com/in/yurifedorov/'
+    },
+    {
+      "icon": "fab fab fa-github",
+      "value": "GitHub",
+      "link": 'https://github.com/yurifyodorov'
+    }
+  ];
 }
