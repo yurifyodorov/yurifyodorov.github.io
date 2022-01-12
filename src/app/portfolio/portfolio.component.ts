b99613e8cf9a32043691115fd95aa180f1241dab
Yuri Fedorov <yuri.inpost@gmail.com>
2022-01-12 11:05:16 +0300

    edit project button

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PortfolioComponent {
+  projects: any[] = [
+    {
+      "name": "Angular Blog",
+      "link": 'https://angular-blog-educational.web.app/'
+    }
+  ];
 }
